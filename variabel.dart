729394c8ba96b780c61a75868f1753cf504d0e85
putrifebrianty <78517974+putrifebrianty@users.noreply.github.com>
2021-06-17 17:26:10 +0700

    Create variabel.dart

---
+++ variabel.dart
@@ -0,0 +1,15 @@
+void main(){
+    // membaut variabel dengan tipe data
+    String nama = "Putri Febrianty";
+    int umur = 21;
+    double tinggi = 155;
+    bool isMenikah = false;
+
+    // membuat veriabel dengan kata kunci var
+    var alamat = "Tanjung Morawa, Sumatera Utara, Indonesia";
+
+    // mencetak variabel
+    print("Nama saya $nama. Umur $umur tahun. Tinggi sekitar $tinggi cm.");
+    print("Menikah: $isMenikah");
+    print("Alamat: $alamat");
+}
